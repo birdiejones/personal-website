2580563e6ab74a33852a9d7bf3820b743173f086
birdiejones <76176698+birdiejones@users.noreply.github.com>
2024-06-13 20:32:06 -0400

    Added success page

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("this is my portfolio")
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
